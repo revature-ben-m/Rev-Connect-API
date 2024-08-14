e9340d375e3e5b7904f189d6fe48306bc12662e8
Zetseon <parthshah3475@gmail.com>
2024-08-14 17:09:33 -0400

    Added CommentRepository, PostRepository

--- rev-connect-api/src/main/java/com/rev_connect_api/repositories/PostRepository.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/repositories/PostRepository.java
@@ -4,4 +4,6 @@ import com.rev_connect_api.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+
 }
