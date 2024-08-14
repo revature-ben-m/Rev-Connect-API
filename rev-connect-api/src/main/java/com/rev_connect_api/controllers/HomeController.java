7ebf31c6a5f03c490a997c2b4b32c20a09c8aadc
sarangi parecha <84612194+sarangi123@users.noreply.github.com>
2024-08-14 10:59:08 -0400

    commit with autowiring
    Added autowiring in service and controller

--- rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
@@ -1,5 +1,15 @@
 package com.rev_connect_api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rev_connect_api.repositories.UserRepository;
+import com.rev_connect_api.services.UserService;
+
+@Service
 public class HomeController {
+	
+	@Autowired
+	UserService userService;
 
 }
--- rev-connect-api/src/main/java/com/rev_connect_api/services/UserService.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/services/UserService.java
@@ -1,5 +1,14 @@
 package com.rev_connect_api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rev_connect_api.repositories.UserRepository;
+
+@Service
 public class UserService {
 
+	@Autowired
+	UserRepository userRepository;
+	
 }
