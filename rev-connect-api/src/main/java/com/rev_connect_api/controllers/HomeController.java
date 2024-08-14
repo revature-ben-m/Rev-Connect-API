c6f462c527b55a333749a89eb330bcb915945e16
Phone Myat <hanminsoe10@gmail.com>
2024-08-14 15:20:23 -0500

    ok

--- rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
@@ -1,5 +1,18 @@
 package com.rev_connect_api.controllers;
 
+<<<<<<< Updated upstream
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rev_connect_api.models.User;
+import com.rev_connect_api.services.UserService;
+
+@RestController
+>>>>>>> Stashed changes
 public class HomeController {
 
 }
