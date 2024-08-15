adbec08c780baac9a93595ea52e85d50dfc06300
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2024-08-15 00:34:28 -0400

    fixed an error with id variable and updated tests

--- rev-connect-api/src/main/java/com/rev_connect_api/models/BusinessProfile.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/models/BusinessProfile.java
@@ -8,7 +8,7 @@ public class BusinessProfile {
     
     @Column(name = "profile_id")
     @Id @GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	private long id;
 
     @Column(name = "user_id", unique = true, nullable = false)
     private long userId;
@@ -24,17 +24,17 @@ public class BusinessProfile {
         this.bioText = bioText;
     }
 
-    public BusinessProfile(Long id, long userId, String bioText) {
+    public BusinessProfile(long id, long userId, String bioText) {
         this.id = id;
         this.userId = userId;
         this.bioText = bioText;
     }
 
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -54,6 +54,19 @@ public class BusinessProfile {
         this.bioText = bioText;
     }
 
+    
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + (int) (userId ^ (userId >>> 32));
+        result = prime * result + ((bioText == null) ? 0 : bioText.hashCode());
+        return result;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -63,10 +76,7 @@ public class BusinessProfile {
         if (getClass() != obj.getClass())
             return false;
         BusinessProfile other = (BusinessProfile) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
+        if (id != other.id)
             return false;
         if (userId != other.userId)
             return false;
