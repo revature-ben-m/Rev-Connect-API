769050e61f59083a7630c7e43f99c43d6ebc11c3
mustardpenguin <andyha234@gmail.com>
2024-08-15 14:16:44 -0400

    Updated post to include data from document and updated tests for that data. Added web config to remove cors

--- rev-connect-api/src/main/java/com/rev_connect_api/models/Post.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/models/Post.java
@@ -2,6 +2,8 @@ package com.rev_connect_api.models;
 
 import jakarta.persistence.*;
 
+import java.math.BigInteger;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
@@ -10,32 +12,120 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer postId;
-    private LocalDateTime createdAt;
-    private String text;
+    private BigInteger postId;
+    private BigInteger userId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private String title;
+    private String content;
 
     public Post() {}
 
-    public Post(int id, String text, LocalDateTime createdAt) {
-        this.postId = id;
-        this.createdAt = createdAt;
-        this.text = text;
-    }
-
-    public Post(String text, LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-        this.text = text;
-    }
-
-    public Integer getPostId() {
+    public BigInteger getPostId() {
         return postId;
     }
 
-    public String getText() {
-        return text;
+    public BigInteger getUserId() {
+        return userId;
     }
 
-    public LocalDateTime getCreatedAt() {
+    public Timestamp getCreatedAt() {
         return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setPostId(BigInteger postId) {
+        this.postId = postId;
+    }
+
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private Post(Builder builder) {
+        postId = builder.postId;
+        userId = builder.userId;
+        createdAt = builder.createdAt;
+        updatedAt = builder.updatedAt;
+        title = builder.title;
+        content = builder.content;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    public static final class Builder {
+        private BigInteger postId;
+        private BigInteger userId;
+        private Timestamp createdAt;
+        private Timestamp updatedAt;
+        private String title;
+        private String content;
+
+        private Builder() {
+        }
+
+        public Builder postId(BigInteger val) {
+            postId = val;
+            return this;
+        }
+
+        public Builder userId(BigInteger val) {
+            userId = val;
+            return this;
+        }
+
+        public Builder createdAt(Timestamp val) {
+            createdAt = val;
+            return this;
+        }
+
+        public Builder updatedAt(Timestamp val) {
+            updatedAt = val;
+            return this;
+        }
+
+        public Builder title(String val) {
+            title = val;
+            return this;
+        }
+
+        public Builder content(String val) {
+            content = val;
+            return this;
+        }
+
+        public Post build() {
+            return new Post(this);
+        }
     }
 }
--- rev-connect-api/src/main/java/com/rev_connect_api/repositories/PostRepository.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/repositories/PostRepository.java
@@ -4,12 +4,13 @@ import com.rev_connect_api.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    Optional<Post> getPostByPostId(int id);
+    Optional<Post> getPostByPostId(BigInteger id);
 
-    void deletePostByPostId(int id);
+    void deletePostByPostId(BigInteger id);
 }
