8a8064ec68fdc7503824dc0e40196103da465471
Matt <themlp@icloud.com>
2024-08-16 13:04:50 -0400

    mattp/trevorl - update HomeController with Cors annotation

--- src/main/java/com/rev_connect_api/controllers/HomeController.java
+++ src/main/java/com/rev_connect_api/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.rev_connect_api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.rev_connect_api.services.PostService;
 import com.rev_connect_api.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HomeController {
     @Autowired
     private UserService userService;
