08e6c94fcbfa252a4ff7bdfe8526e13578c8ebfa
Matt <themlp@icloud.com>
2024-08-18 10:55:55 -0400

    mattp - remove circular dependency

--- src/main/java/com/rev_connect_api/config/SecurityConfig.java
+++ src/main/java/com/rev_connect_api/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.rev_connect_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.rev_connect_api.security.JWTAuthenticationFilter;
-import com.rev_connect_api.services.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -22,25 +20,21 @@ public class SecurityConfig {
 
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
     
-    @Lazy
-    private final UserService userService;
-
-    public SecurityConfig(JWTAuthenticationFilter jwtAuthenticationFilter, @Lazy UserService userService) {
+    public SecurityConfig(JWTAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.userService = userService;
     }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/login", "/users/register").permitAll() // allow login and register to everyone 
-                        .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN") // USER or ADMIN roles can access /users/**
-                        .anyRequest().anonymous()) // all other requests require authentication
-                        .sessionManagement(session -> session
-                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                        .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class); 
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/auth/login", "/users/register").permitAll() // allow login and register to everyone 
+                .requestMatchers("/users/**", "/posts/**", "/comments/**", "/likes/**").hasAnyRole("USER", "ADMIN") // USER or ADMIN roles can access /users/**
+                .anyRequest().anonymous()) // all other requests require authentication
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class); 
+            
         return http.build();
     }
 
