48a2dc1df97beccc7c09fcb462210cf3745b1d19
joseph941230revature <joseph941230@hotmail.com>
2024-08-18 08:26:40 -0700

    backend login
    Signed-off-by: joseph941230revature <joseph941230@hotmail.com>

--- rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.rev_connect_api.controllers;
 
 import com.rev_connect_api.entity.User;
+import com.rev_connect_api.models.UserDTO;
 import com.rev_connect_api.services.UserService;
+import com.rev_connect_api.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/home")
 public class HomeController {
@@ -20,13 +24,44 @@ public class HomeController {
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody User user) {
         try {
+            //loginAccount check from database
             User loginAccount = userService.login(user);
             loginAccount.setPassword(null);
-            return new ResponseEntity<>(loginAccount, HttpStatus.OK);
+
+            // username
+            String username = loginAccount.getUsername();
+            Integer accountId = loginAccount.getAccountId();
+            // generate token
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("username",username);
+            claims.put("accountId",accountId);
+            String token = JwtUtil.generateToken(claims);
+
+            //DTO send to frond end
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(loginAccount.getUsername());
+            userDTO.setEmail(loginAccount.getEmail());
+            userDTO.setAccountId(loginAccount.getAccountId());
+            userDTO.setToken(token);
+
+            //send to front
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<Object> getUserInfo(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        if(token == null || token.isEmpty()){
+            return new ResponseEntity<>("请登录后再使用", HttpStatus.UNAUTHORIZED);
+        }
+
+        //如果有token   校验一下 过期时间
+        //fanhui用户信息
+        UserDTO userDTO = new UserDTO();
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
 
 }
--- rev-connect-api/src/main/java/com/rev_connect_api/models/UserDTO.java
+++ rev-connect-api/src/main/java/com/rev_connect_api/models/UserDTO.java
@@ -1,10 +1,42 @@
 package com.rev_connect_api.models;
 
 
-
-
 public class UserDTO {
     private String username;
-    private String password;
+    private Integer accountId;
+    private String email;
 
+    private String token;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
